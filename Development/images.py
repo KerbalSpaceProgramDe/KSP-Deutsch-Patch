e0f6a484f2f74ec53d27560c14d51bff93721904
Dorian Stoll <dorian@tmsp.ga>
2016-10-20 16:26:16 +0200

    Bilder

---
+++ Development/images.py
@@ -0,0 +1,47 @@
+﻿# images.py - Generates an image config for KDP
+# Copyright (c) 2016 Thomas P.
+# Do what you want
+
+# Imports
+import glob
+import sys
+import os
+import os.path
+
+# The final config
+config = """// **************
+// KSP-Deutsch-Patch: Übersetzt KSP ins Deutsche
+// Copyright (c) 2016 kerbalspaceprogram.de
+// 
+// Images.cfg: Ersetzt die Spielgrafiken die Texte enthalten
+// Autor: Von image.py generiert
+// **************
+
+LANGUAGEPATCHES
+{
+"""
+
+# Go through all image files
+for filename in glob.glob(sys.argv[1] + '/*'):
+	# Log the file
+	print('[IMAGE] Found image ' + filename)
+	
+	# add config
+	name = os.path.basename(filename).split('.')[0]
+	config += """	IMAGE
+	{
+		name = """ + name + """
+		file = """ + sys.argv[2] + name + """
+	}
+"""
+
+# Finish the file and save it
+config += '}'
+
+if os.path.exists("Images.cfg"):
+	file = open("Images.cfg", "r+")	
+else:
+	file = open("Images.cfg", "w")
+file.seek(0)
+file.write(config)
+file.close()
